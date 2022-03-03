d10b5d1037c9123193c7eb3970a77a2030c56db3
Muskan-Gandhi <muskangandhi73@gmail.com>
2022-03-03 15:34:20 +0530

    product component createdp

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -3,28 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import { ProductComponent } from './product/product.component';
-=======
+
 import { BuyerComponent } from './buyer/buyer.component';
->>>>>>> 9f2c39e01891fb53b2d95bea4ff07edfb2186c89
-=======
+
+
 import { CustomerComponent } from './customer/customer.component';
->>>>>>> 08ddf4d383a4693576107c6c4c005a37aed9f2c7
+
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
-    ProductComponent
-=======
-    BuyerComponent
->>>>>>> 9f2c39e01891fb53b2d95bea4ff07edfb2186c89
-=======
-    CustomerComponent
->>>>>>> 08ddf4d383a4693576107c6c4c005a37aed9f2c7
+    ProductComponent,
+    BuyerComponent,
+    CustomerComponent,
   ],
   imports: [
     BrowserModule,
