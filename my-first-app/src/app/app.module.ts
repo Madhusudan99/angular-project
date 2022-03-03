ffa9abdd93c58917c907b8e32d96eec0c83c5e31
MrAwsm99 <rajaksujitkumar99@gmail.com>
2022-03-03 15:44:40 +0530

    Form component created

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -13,6 +13,7 @@ import { CustomerComponent } from './customer/customer.component';
 <<<<<<< HEAD
 <<<<<<< HEAD
 import { SellerComponent } from './seller/seller.component';
+import { FormComponent } from './form/form.component';
 =======
 >>>>>>> 08ddf4d383a4693576107c6c4c005a37aed9f2c7
 >>>>>>> 9f2c39e01891fb53b2d95bea4ff07edfb2186c89
@@ -26,7 +27,8 @@ import { SellerComponent } from './seller/seller.component';
 <<<<<<< HEAD
 <<<<<<< HEAD
     CustomerComponent,
-    SellerComponent
+    SellerComponent,
+    FormComponent
 =======
 <<<<<<< HEAD
     BuyerComponent
