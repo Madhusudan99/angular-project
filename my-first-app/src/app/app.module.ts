b9a030d723e9d6ce6918753d5a9c9e2964837571
MrAwsm99 <rajaksujitkumar99@gmail.com>
2022-03-03 15:47:11 +0530

    app.module.ts modified

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -10,37 +10,19 @@ import { BuyerComponent } from './buyer/buyer.component';
 
 
 import { CustomerComponent } from './customer/customer.component';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { SellerComponent } from './seller/seller.component';
 import { FormComponent } from './form/form.component';
-=======
->>>>>>> 08ddf4d383a4693576107c6c4c005a37aed9f2c7
->>>>>>> 9f2c39e01891fb53b2d95bea4ff07edfb2186c89
-=======
-
->>>>>>> d10b5d1037c9123193c7eb3970a77a2030c56db3
-
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
     CustomerComponent,
     SellerComponent,
-    FormComponent
-=======
-<<<<<<< HEAD
-    BuyerComponent
-=======
-    CustomerComponent
->>>>>>> 08ddf4d383a4693576107c6c4c005a37aed9f2c7
->>>>>>> 9f2c39e01891fb53b2d95bea4ff07edfb2186c89
-=======
+    FormComponent,
+    BuyerComponent,
+    CustomerComponent,
     ProductComponent,
     BuyerComponent,
     CustomerComponent,
->>>>>>> d10b5d1037c9123193c7eb3970a77a2030c56db3
   ],
   imports: [
     BrowserModule,
