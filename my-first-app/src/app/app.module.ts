b18410d9db81eaeb2e236f322aef7c873cfa1bd0
MrAwsm99 <rajaksujitkumar99@gmail.com>
2022-03-03 15:23:45 +0530

    Buyer component created

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BuyerComponent } from './buyer/buyer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuyerComponent
   ],
   imports: [
     BrowserModule,
