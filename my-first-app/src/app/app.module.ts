3fa6584347cd2785750e471fee99cfdafc6617c3
Naynesh335 <nayneshchaudhary@gmail.com>
2022-03-03 23:38:06 +0530

    customer Form completed

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
@@ -19,11 +22,14 @@ import { FormComponent } from './form/form.component';
     BuyerComponent,
     CustomerComponent,
     PageNotFoundComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- my-first-app/src/app/form/form.component.ts
+++ my-first-app/src/app/form/form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormBuilder,  Validators, ReactiveFormsModule } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -10,23 +11,25 @@ export class FormComponent implements OnInit {
 
   constructor(private fb: FormBuilder) { }
 
-  CustomerForm:any;
+  myForm:any;
   ngOnInit(): void {
 
-    this.CustomerForm = this.fb.group({
+    this.myForm = this.fb.group({
       FirstName:['',Validators.required],
       LastName: ['', Validators.required],
+      Birthdate: ['', Validators.required],
       CurrentAddress: ['', Validators.required],
+      CurrenthouseNo: ['', Validators.required],
       CurrentCity: ['', Validators.required],
       CurrentState:[ '', Validators.required],
       CurrentZip:['',Validators.required],
       ParmanentAddress: ['', Validators.required],
+      ParmanenthouseNo: ['', Validators.required],
       ParmanentCity: ['', Validators.required],
       ParmanentState: ['', Validators.required],
       ParmanentZip:[ '', Validators.required],
       Email:[ '', [Validators.required,Validators.email]],
-      PhoneNumber:[ '', [Validators.required,Validators.minLength(10),Validators.maxLength(10)]],
-      
+      PhoneNumber:[ '', [Validators.required,Validators.minLength(10),Validators.maxLength(10)]]
     });
   }
 
