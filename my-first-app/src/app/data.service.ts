e46eb39a3320ab16ed4c70dd42c0d43e98494ba4
Madhusudan Jha <madhusudanjha14071999@gmail.com>
2022-03-04 14:26:10 +0530

    ip - fix

--- my-first-app/src/app/data.service.ts
+++ my-first-app/src/app/data.service.ts
@@ -9,10 +9,10 @@ export class DataService {
   constructor(private http: HttpClient) { }
 
   urls = {
-    "buyerData": "http://localhost:3000/buyer_details",
-    "customerData": "http://localhost:3000/customer_details",
-    "loginData": "http://localhost:3000/login_details",
-    "sellerData": "http://localhost:3000/seller_name"
+    "buyerData": "http://192.168.2.85:3000/buyer_details/",
+    "customerData": "http://192.168.2.85:3000/customer_details/",
+    "loginData": "http://192.168.2.85:3000/login_details/",
+    "sellerData": "http://192.168.2.85:3000/seller_name/"
   }
 
 
