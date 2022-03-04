a207cd59097e3f4a07951b589a33d831ee8306cc
Madhusudan Jha <madhusudanjha14071999@gmail.com>
2022-03-04 06:37:26 +0530

    Integration of load and insert feature on customer

--- my-first-app/src/app/customer/customer.component.ts
+++ my-first-app/src/app/customer/customer.component.ts
@@ -14,7 +14,27 @@ export class CustomerComponent implements OnInit {
   constructor(private dS: DataService, private modalService: NgbModal, private fb: FormBuilder) { }
 
   customerList: any;
-  myForm:any;
+  myForm: any;
+  formIsNew = false;
+  emptyForm = {
+    "first_name": "",
+    "last_name": "",
+    "date_of_birth": "",
+    "current_address": {
+      "houseno": "",
+      "street": "",
+      "city": "",
+      "zipcode": ""
+    },
+    "permenant_address": {
+      "houseno": "",
+      "street": "",
+      "city": "",
+      "zipcode": ""
+    },
+    "email": "",
+    "phone_number": ""
+  }
 
 
   addToCustomerList(data: any) {
@@ -49,42 +69,54 @@ export class CustomerComponent implements OnInit {
     console.log(this.myForm.value);
   }
 
-  currentDate:any;
-  objectToFormData(objData:any) {
+  // currentDate:any;
+  objectToFormData(objData: any) {
     // this.myForm.FirstName = objData.first_name;
     // this.myForm.LastName = objData.last_name;
-    this.currentDate = new Date(Number(objData.date_of_birth.split("-")[0]), Number(objData.date_of_birth.split("-")[1]), Number(objData.date_of_birth.split("-")[2]));
-    console.log(objData.date_of_birth)
-    this.myForm.setValue({ 
-      FirstName: objData.first_name, 
+    // this.currentDate = new Date(Number(objData.date_of_birth.split("-")[0]), Number(objData.date_of_birth.split("-")[1]), Number(objData.date_of_birth.split("-")[2]));
+    // console.log(objData.date_of_birth)
+    this.myForm.setValue({
+      FirstName: objData.first_name,
       LastName: objData.last_name,
-      Birthdate: this.currentDate,
+      Birthdate: objData.date_of_birth,
       CurrentAddress: objData.current_address.street,
-      CurrenthouseNo: objData.last_name,
-      CurrentCity: objData.last_name,
-      CurrentState: objData.last_name,
-      CurrentZip: objData.last_name,
-      ParmanentAddress: objData.last_name,
-      ParmanenthouseNo: objData.last_name,
-      ParmanentCity: objData.last_name,
-      ParmanentState: objData.last_name,
-      ParmanentZip: objData.last_name,
-      Email: objData.last_name,
-      PhoneNumber: objData.last_name,
+      CurrenthouseNo: objData.current_address.houseno,
+      CurrentCity: objData.current_address.city,
+      CurrentState: objData.current_address.city,
+      CurrentZip: objData.current_address.zipcode,
+      ParmanentAddress: objData.permenant_address.street,
+      ParmanenthouseNo: objData.permenant_address.houseno,
+      ParmanentCity: objData.permenant_address.city,
+      ParmanentState: objData.permenant_address.city,
+      ParmanentZip: objData.permenant_address.zipcode,
+      Email: objData.email,
+      PhoneNumber: objData.phone_number,
 
     });
     // console.log(this.myForm.FirstName.value)
     // console.log(this.myForm.LastName)
-    console.log(this.currentDate);
+    // console.log(this.currentDate);
   }
 
   closeResult = '';
-  open(content: any, data:any) {
+  open(content: any, data: any) {
+
     this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', size: 'xl', backdrop: 'static' }).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
       this.closeResult = `Dismissed`;
     });
-    this.objectToFormData(data);
+
+
+    console.log(data)
+    if (data) {
+      this.formIsNew = false;
+      this.objectToFormData(data);
+    }
+    else {
+      this.formIsNew = true;
+      this.objectToFormData(this.emptyForm);
+    }
+
   }
 }
