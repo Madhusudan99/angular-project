da18bb588cd6098e25dac487f28be7aa5bfa68bb
Madhusudan Jha <madhusudanjha14071999@gmail.com>
2022-03-05 09:45:47 +0530

    product update and delete working

--- my-first-app/src/app/data.service.ts
+++ my-first-app/src/app/data.service.ts
@@ -12,7 +12,8 @@ export class DataService {
     "buyerData": "http:///192.168.43.34.78:3000/buyer_details/",
     "customerData": "http:///192.168.43.34:3000/customer_details/",
     "loginData": "http:///192.168.43.34.78:3000/login_details/",
-    "sellerData": "http:///192.168.43.34:3000/seller_details/"
+    "sellerData": "http:///192.168.43.34:3000/seller_details/",
+    "productData": "http:///192.168.43.34:3000/product/"
   }
 
 
@@ -44,5 +45,18 @@ export class DataService {
   }
   postSellerData(data: any) {
     return this.http.post(this.urls.sellerData, data);
+  }
+
+  getProductData() {
+    return this.http.get(this.urls.productData);
+  }
+  putProductData(data:any) {
+    return this.http.put(this.urls.productData+data.id, data);
+  }
+  deleteProductData(id:any) {
+    return this.http.delete(this.urls.productData+id);
+  }
+  postProductData(data: any) {
+    return this.http.post(this.urls.productData, data);
   }
 }
